69b6d0ad4ecfe4553f258e7106b0d94e1e710141
Denis Vyatkin <catinjar42@gmail.com>
2019-11-24 19:12:08 +0500

    Basic film searching

--- MyTorrentzBot/Program.cs
+++ MyTorrentzBot/Program.cs
@@ -1,142 +1,158 @@
 ﻿using System;
-using Telegram.Bot;//Библиотека Telegram.Bot
-using System.Threading.Tasks;//библиотека для ассинхронных потоков
-using System.Data.SQLite;//Библиотека поддержики SQLite баз данных
-using System.Net;//Получение ссылок со страницы сайта
-using MihaZupan;//Обход блокировки телеграм через прокси4   
+using Telegram.Bot;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using Telegram.Bot.Types.Enums;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
 namespace MyTorrentzBot
 {
     class Program
     {
-        const string TOKEN = "858759117:AAHPjdQEW1NScJlVnZRjKFdccEr0SopRoeM";//переменная с токеном бота типа константа. TOKEN с ошибкой, так как ещё его не использовали
-        public static SQLiteConnection DB;//Переменная с базой данных
-        static void Main(string[] args)
+        private const string BotToken = "858759117:AAHPjdQEW1NScJlVnZRjKFdccEr0SopRoeM";
+
+        private static TelegramBotClient bot;
+
+        private static SQLiteConnection usersDb;
+        private static SQLiteConnection torrentsDb;
+
+        private static void Main()
         {
+            bot = new TelegramBotClient(BotToken);
+
+            usersDb = new SQLiteConnection("Data Source=users.db; Version=3");
+            usersDb.Open();
+
+            torrentsDb = new SQLiteConnection("Data Source=torrents.db3; Version=3");
+            torrentsDb.Open();
+
             while (true)
             {
-                try//обработчик  исключений. Отлавливает исключения, вызванные в ходе выполнения работы
+                try
                 {
-                    GetMessage().Wait();//здесь будет основная задача бота, метод
+                    HandleMessages().Wait();
                 }
-                catch (Exception ex)//здесь ошибка, если срабатывает исключение. Выводит в консоль
+                catch (Exception ex)
                 {
                     Console.WriteLine("Error: " + ex);
                 }
             }
         }
-        static async Task GetMessage()//метод, выполняющие авторизацию и получающий обновления от телеграм бота. Он статический и ассинхронный. Второй поток типа
+
+        private static async Task HandleMessages()
         {
-            var socksProxy = new HttpToSocks5Proxy("183.102.71.77", 8888);
-            TelegramBotClient bot = new TelegramBotClient(TOKEN, socksProxy);//Переменная авторизации бота.
-            int offset = 0;//тупа нужно
+            int offset = 0; // тупа нужно
             int timeout = 0;
-            try
+
+            await bot.SetWebhookAsync(""); //Убираем вебхук
+
+            while (true)
             {
-                await bot.SetWebhookAsync("");//Убираем вебхук
-                while (true)//Вытаскиваем сообщения из обновления бота
+                var updates = await bot.GetUpdatesAsync(offset, timeout); //Получили обновления
+
+                foreach (var update in updates)
                 {
-                    var updates = await bot.GetUpdatesAsync(offset, timeout);//Получили обновления
-                    foreach (var update in updates)//
+                    if (update.Type != UpdateType.Message) // Может быть и не сообщение
+                        continue;
+
+                    var message = update.Message;
+                    var words = message.Text.Split().ToList();
+
+                    var command = words[0];
+                    var args = words.Count > 1 ? words.GetRange(1, words.Count - 1) : new List<string>();
+
+                    switch (command)
                     {
-                        var message = update.Message;//Получили сообщение
-                        /*if (message.Text == "MyFirstBot")//проверяем содержимое сообщения
-                        {
-                            Console.WriteLine("Получено сообщение: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");//Вывели сообщение в консоль
-                            await bot.SendTextMessageAsync(message.Chat.Id, "Привет, создатель, я твой бот!");//ответ на сообщение
+                        case "/start":
+                            SignUpUser(message.Chat.Id.ToString(), message.Chat.Username.ToString());
+                            await bot.SendTextMessageAsync(message.Chat.Id, "Приветствую, я я Торрент бот, который облегчит тебе поиски фильмов, которые ты так хочешь посмотреть.\nВсе мои команды можешь узнать с помощью /help");
+                            Console.WriteLine($"Новый пользователь: {message.Chat.Username}; ID чата: {message.Chat.Id}; Дата,время(GMT 0): {DateTime.UtcNow}\n");
+                            break;
+
+                        case "MyFirstBot":
+                            Console.WriteLine($"Получено сообщение: {message.Text}; ID отправителя: {message.Chat.Username}; ID чата: {message.Chat.Id}; Дата,время(GMT 0): {DateTime.UtcNow}\n");
+                            await bot.SendTextMessageAsync(message.Chat.Id, "Привет, создатель, я твой бот!");
                             await bot.SendTextMessageAsync(message.Chat.Id, "Сообщение отправлено пользователем - " + message.Chat.Username);
-                        }//MyFirstBot
-                        if (message.Text == "/reg")
-                        {
+                            break;
+
+                        case "/help":
+                            Console.WriteLine($"Получена команда: {message.Text}; ID отправителя: {message.Chat.Username}; ID чата: {message.Chat.Id}; Дата,время(GMT 0): {DateTime.UtcNow}\n");
+                            await bot.SendTextMessageAsync(message.Chat.Id, "Привет, сейчас я покажу тебе все свои возможности...");
+                            await bot.SendTextMessageAsync(message.Chat.Id, "/find - позволит начать процесс поиска нужного тебе фильма;");
+                            break;
+
+                        /*case "/reg":
                             Registration(message.Chat.Id.ToString(), message.Chat.Username.ToString());
                             await bot.SendTextMessageAsync(message.Chat.Id, "Пользователь зарегистрирован");
                             Console.WriteLine("Зарегистрирован пользователь с ID: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + "  ; Дата, время(GMT 0): " + DateTime.UtcNow + "\n");
-                        }// /reg
-                        if (message.Text == "/help")
-                        {
-                            Console.WriteLine("Получено сообщение: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                            await bot.SendTextMessageAsync(message.Chat.Id, "Привет, сейчас я покажу тебе все свои возможности...");
-                            await bot.SendTextMessageAsync(message.Chat.Id, "/reg - позволит тебе зарегистрироваться у нас, чтобы в дальнейшем получать рекомендации по самым лучшим фильмам, которые могут тебе понравится;");
+                            break;*/
 
-                        }// /help*/
+                        case "/find":
+                            var filmTitle = string.Join(' ', args);
 
-                        switch (message.Text)
-                        {
-                            case "/start":
-                                Registration(message.Chat.Id.ToString(), message.Chat.Username.ToString());
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Приветствую, я я Торрент бот, который облегчит тебе поиски фильмов, которые ты так хочешь посмотреть.\nВсе мои команды можешь узнать с помощью /help");
-                                Console.WriteLine("Новый пользователь: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                break;
-                            case "MyFirstBot":
-                                Console.WriteLine("Получено сообщение: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Привет, создатель, я твой бот!");
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Сообщение отправлено пользователем - " + message.Chat.Username);
-                                break;
-                            case "/help":
-                                Console.WriteLine("Получена команда: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Привет, сейчас я покажу тебе все свои возможности...");
-                                await bot.SendTextMessageAsync(message.Chat.Id, "/find - позволит начать процесс поиска нужного тебе фильма;");
-                                break;
-                            /*case "/reg":
-                                Registration(message.Chat.Id.ToString(), message.Chat.Username.ToString());
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Пользователь зарегистрирован");
-                                Console.WriteLine("Зарегистрирован пользователь с ID: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + "  ; Дата, время(GMT 0): " + DateTime.UtcNow + "\n");
-                                break;*/
-                            case "/find":
-                                Console.WriteLine("Получена команда: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                await bot.SendTextMessageAsync(message.Chat.Id, "Напиши название фильма, который ты хочешь найти");
-                                string messageTextOne = message.Text;
-                                string nameFilm = null;
-                                while (true)
+                            Console.WriteLine($"Command: {command}; User: {message.Chat.Username}; Chat ID: {message.Chat.Id}; Time (GMT 0): {DateTime.UtcNow}\n; Film title: {filmTitle}");
+
+                            var regcmd = torrentsDb.CreateCommand();
+                            regcmd.CommandText = $"SELECT hash_info, title FROM torrent WHERE title LIKE '%{filmTitle}%' LIMIT 5";
+                            regcmd.Parameters.AddWithValue("@film_title", "Крестный отец");
+
+                            var dbReader = regcmd.ExecuteReader();
+                            if (dbReader.HasRows)
+                            {
+                                var sb = new StringBuilder();
+
+                                sb.AppendLine("Вот, что нашлось:");
+
+                                while (dbReader.Read())
                                 {
-                                    var up = await bot.GetUpdatesAsync(offset, timeout);
-                                    foreach (var upp in up)
-                                    {
-                                        message = upp.Message;
-                                    }
-                                    if (messageTextOne != message.Text)
-                                    {
-                                        await bot.SendTextMessageAsync(message.Chat.Id, "Сейчас глянем, что у нас есть по запросу " + message.Text);
-                                        Console.WriteLine("Получено сообщение: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                        nameFilm = message.Text;
-                                        // Поиск по качеству фильма И выдёргивание торрент файла, крч всё осноное здесь должно быть
-                                        message.Text = "С этим разобрались. Жду твоих команд, друг";
-                                        await bot.SendTextMessageAsync(message.Chat.Id, message.Text);
-                                        break;
-                                    }
-                                }
-                                break;
-                            default:
-                                Console.WriteLine("Получено незарегистрированное сообщение: " + message.Text + " ; ID отправителя: " + message.Chat.Username + " ; ID чата: " + message.Chat.Id + " ; Дата,время(GMT 0): " + DateTime.UtcNow + "\n");
-                                //await bot.SendTextMessageAsync(message.Chat.Id, "Я не знаю такой команды. Воспользуйтесь функцией /help");
-                                break;
-                        }
+                                    var description = dbReader.GetString(1);
+                                    var magnetHash = dbReader.GetString(0);
 
-                        offset = update.Id + 1;//чтобы не приходило много обновлений
+                                    sb.AppendLine($"{description} **mаgnet:?xt=urn:btih:{dbReader.GetString(0)}**");
+                                    sb.AppendLine();
+                                }
+
+                                await bot.SendTextMessageAsync(message.Chat.Id, sb.ToString());
+                            }
+                            else
+                            {
+                                await bot.SendTextMessageAsync(message.Chat.Id, "Ничего не нашел. Попробовать еще раз?");
+                            }
+
+                            break;
+
+                        default:
+                            Console.WriteLine($"Получено незарегистрированное сообщение: {message.Text}; ID отправителя: {message.Chat.Username}; ID чата: {message.Chat.Id}; Дата,время(GMT 0): {DateTime.UtcNow}\n");
+                            //await bot.SendTextMessageAsync(message.Chat.Id, "Я не знаю такой команды. Воспользуйтесь функцией /help");
+                            break;
                     }
+
+                    offset = update.Id + 1; //чтобы не приходило много обновлений
                 }
+            }
+        }
+
+        public static void SignUpUser(string chatId, string username) // Метод для регистрации пользователя в базе данных
+        {
+            try
+            {
+                usersDb = new SQLiteConnection("Data Source=DB.db; Version=3");
+                usersDb.Open();
+                
+                var regcmd = usersDb.CreateCommand();//Создаём команду добавления пользователя
+                regcmd.CommandText = "INSERT INTO Regusers VALUES(@chatId, @username)";//Запрос на добавление пользователя
+                regcmd.Parameters.AddWithValue("@chatId", chatId);//Добавляем параметры
+                regcmd.Parameters.AddWithValue("@username", username);
+                regcmd.ExecuteNonQuery();//Подставить и выполнить
+                
+                usersDb.Close();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex);
             }
         }
-        public static void Registration(string chatId, string username)//Метод для регистрации пользователя в базе данных
-        {
-            try
-            {
-                DB = new SQLiteConnection("Data Source=DB.db; Version=3");//Присваиваем переменной значение базы данных
-                DB.Open();//Открыли базу данных
-                SQLiteCommand regcmd = DB.CreateCommand();//Создаём команду добавления пользователя
-                regcmd.CommandText = "INSERT INTO Regusers VALUES(@chatId, @username)";//Запрос на добавление пользователя
-                regcmd.Parameters.AddWithValue("@chatId", chatId);//Добавляем параметры
-                regcmd.Parameters.AddWithValue("@username", username);
-                regcmd.ExecuteNonQuery();//Подставить и выполнить
-                DB.Close();//Закрыли базу данных
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex);
-            }
-        }// /reg
     }
 }
